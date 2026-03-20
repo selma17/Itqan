a01ca5fe8428a706908a1604858e4ef1c9d034dc
Selma Soued <selmasouedsd@gmail.com>
2026-03-20 21:36:25 +0100

    feat: mushaf reading screen with manual lines system

---
+++ src/data/mushaflinesIndex.js
@@ -0,0 +1,18 @@
+// AUTO-GENERATED
+export default {
+  590: require('./mushaf-lines/page-590.json'),
+  591: require('./mushaf-lines/page-591.json'),
+  592: require('./mushaf-lines/page-592.json'),
+  593: require('./mushaf-lines/page-593.json'),
+  594: require('./mushaf-lines/page-594.json'),
+  595: require('./mushaf-lines/page-595.json'),
+  596: require('./mushaf-lines/page-596.json'),
+  597: require('./mushaf-lines/page-597.json'),
+  598: require('./mushaf-lines/page-598.json'),
+  599: require('./mushaf-lines/page-599.json'),
+  600: require('./mushaf-lines/page-600.json'),
+  601: require('./mushaf-lines/page-601.json'),
+  602: require('./mushaf-lines/page-602.json'),
+  603: require('./mushaf-lines/page-603.json'),
+  604: require('./mushaf-lines/page-604.json'),
+};
